f2c3593b3da695c5683a7ee71f72c7249632886d
jepjr-cloud <pasquinijep@gmail.com>
2021-03-02 17:47:02 -0300

    Created ebs-module directory, started implementating resources

---
+++ modules/ebs-module/main.tf
@@ -0,0 +1,9 @@
+resource "aws_ebs_volume" "ebs" {
+  type              = var.type
+  size              = var.size
+  availability_zone = var.availability_zone
+
+  tags = {
+    "Name" = var.name + "-ebs"
+  }
+}
---
+++ modules/ebs-module/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The AZ to start the instance in"
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "size" {
+  description = "Size of the EBS volume"
+  type = string
+  default = "35"
+}
