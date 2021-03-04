72d06947e168f9a80bdfe5fe4d1ca8014050e860
jepjr-cloud <pasquinijep@gmail.com>
2021-03-04 17:32:07 -0300

    Added outputs for cloudwatch-module

--- TF-lab/outputs.tf
+++ TF-lab/outputs.tf
@@ -41,4 +41,16 @@ output "ebs_size" {
   description = "size of the ebs volume (gb)"
 }
 
+#--------------------------------------------------
+# CloudWatch
+#--------------------------------------------------
 
+output "cw_alarm_ec2_id" {
+  value = module.metric_alarm.cloudwatch_metric_alarm_id
+  description = "The ID of the Cloudwatch metric alarm."
+}
+
+output "cw_alarm_ec2_arn" {
+  value = module.metric_alarm.cloudwatch_metric_alarm_arn
+  description = "The ARN of the Cloudwatch metric alarm."
+}
